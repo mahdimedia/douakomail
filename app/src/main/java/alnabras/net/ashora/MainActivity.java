fab5b5e4753ca34643fece9cfd2becdf21507bfa
mahdimedia <ali_montdyat@hotmail.com>
2019-10-15 06:04:07 +0300

    update final

--- app/src/main/java/alnabras/net/ashora/MainActivity.java
+++ app/src/main/java/alnabras/net/ashora/MainActivity.java
@@ -1,35 +1,35 @@
 package alnabras.net.ashora;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import android.view.View;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
-
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private AdView mAdView;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,39 +37,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_dua_alqamah,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.zayart_forty, R.id.nav_share, R.id.nav_send, R.id.moreapp )
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-    }
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+        }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -77,4 +74,58 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() != R.id.action_settings) {
+            return super.onOptionsItemSelected(item);
+        }
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(R.string.about);
+        alertDialog.setMessage(getString(R.string.version_app));
+        alertDialog.setButton(-3, (getString(R.string.ok)), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+        return true;
+
+    }
+
+   // public void rate_app(View view) {
+   //     Intent intent = new Intent("android.intent.action.VIEW");
+   //     intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.mhdamz.ad3iya.ta3kibat.p1"));
+  //      startActivity(intent);
+ //   }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.moreapp) {
+
+
+            Intent intent = new Intent("android.intent.action.VIEW");
+            intent.setData(Uri.parse("https://play.google.com/store/apps/developer?id=AYASOFT"));
+            startActivity(intent);
+
+
+    } else
+            if (id == R.id.share_whatsapp) {
+                return true;
+
+
+        } else
+            if (id == R.id.nav_send) {
+
+        } else
+            if (id == R.id.nav_share) {
+
+            }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
